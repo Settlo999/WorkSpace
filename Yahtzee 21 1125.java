d82946f44a87fbecfac5b5368d4576aaa3b82a75
Settlo999 <94993553+Settlo999@users.noreply.github.com>
2021-12-24 21:15:22 +0900

    Update Yahtzee 21 1125.java
    添削反映版

--- Yahtzee 21 1125.java
+++ Yahtzee 21 1125.java
@@ -2,25 +2,18 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Test {
 
 	public static void main(String[] args) {
-		
 		System.out.println("ゲームスタート");
 		//改行
 		System.out.println(); 
 		
-		//ダイス1～5
-		int d1 = makeDice(); 
-		int d2 = makeDice();
-		int d3 = makeDice();
-		int d4 = makeDice();
-		int d5 = makeDice();
+		int[] dices = new int[5];
 		
-		//配列に
-		int[] dices = {d1, d2, d3, d4, d5}; 
-		//昇順に
-		Arrays.sort(dices); 
+		for(int i = 0; i < 5; i++) {
+			dices[i] = makeDice();
+		}
 		
 		//出目
 		System.out.println("現在の出目" + Arrays.toString(dices)); 
@@ -32,40 +25,28 @@ public class Main {
 		
 		//振り直しのカウント
 		int retryCount = 0; 
-		while (true) {
+		for(int i = 0; i < 2; i++) {
 			Scanner scanner = new Scanner(System.in);
 			//振り直しか終了の判定用
-			String rertyOrFin = scanner.next(); 
+			String retryOrFin = scanner.next(); 
 			
-			//judgeへ
-			if(rertyOrFin.equals("f")) { 
+			//judgeへ 定数=変数にするべし
+			if("f".equals(retryOrFin)) { 
 				break;
 			}
 			else {
-				//1番目だけ振り直し
-				if (rertyOrFin.contains("1")) { 
-					dices[0] = makeDice();
-				}
-				if (rertyOrFin.contains("2")) {
-					dices[1] = makeDice();
-				}
-				if (rertyOrFin.contains("3")) {
-					dices[2] = makeDice();
-				}
-				if (rertyOrFin.contains("4")) {
-					dices[3] = makeDice();
-				}
-				if (rertyOrFin.contains("5")) {
-					dices[4] = makeDice();
+				String[] nums = {"1", "2", "3", "4", "5"};
+				//振り直し
+				for(int j = 0; j < 5; j++) {
+					if (retryOrFin.contains(nums[j])) {
+						dices[j] = makeDice();
+					}
 				}
 				
-				//整列し直し
-				Arrays.sort(dices); 
 				System.out.println("現在の出目" + Arrays.toString(dices));
 				//回数追加
 				retryCount++; 
 			}
-			
 			//振り直し2回目なら終了してjudgeへ
 			if(retryCount == 2) { 
 				break;
@@ -89,106 +70,85 @@ public class Main {
          * @param int[] dices 出目の配列
          */
 	public static void judge(int[] dices) {
-		//1の目のカウント
-		int oneCount = 0; 
-		int twoCount = 0;
-		int threeCount = 0;
-		int fourCount = 0;
-		int fiveCount = 0;
-		int sixCount = 0;
+		//目のカウント
+		int[] numCounts = {0, 0, 0, 0, 0, 0};
 		
 		//それぞれカウント
-		for(int i = 0; i < dices.length; i++) { 
-			if(dices[i] == 1) {
-				oneCount++;
-			}
-			if(dices[i] == 2) {
-				twoCount++;
-			}
-			if(dices[i] == 3) {
-				threeCount++;
-			}
-			if(dices[i] == 4) {
-				fourCount++;
-			}
-			if(dices[i] == 5) {
-				fiveCount++;
-			}
-			if(dices[i] == 6) {
-				sixCount++;
-			}
-		}
-		
-		//1の目の合計
-		if(oneCount >= 1) {
-			System.out.println("1の目: " + (1 * oneCount)); 
-		}
-		if(twoCount >= 1) {
-			System.out.println("2の目: " + (2 * twoCount));
-		}
-		if(threeCount >= 1) {
-			System.out.println("3の目: " + (3 * threeCount));
-		}
-		if(fourCount >= 1) {
-			System.out.println("4の目: " + (4 * fourCount));
-		}
-		if(fiveCount >= 1) {
-			System.out.println("5の目: " + (5 * fiveCount));
-		}
-		if(sixCount >= 1) {
-			System.out.println("6の目: " + (6 * sixCount));
-		}
-		
-		//出目の和
-		System.out.println("チャンス: " + (1 * oneCount + 2 * twoCount + 
-				3 * threeCount + 4 * fourCount + 5 * fiveCount + 6 * sixCount)); 
-		
-		//同じ出目が3つ以上なら出目の和
-		if(oneCount >= 3 || twoCount >= 3 || threeCount >= 3 || 
-				fourCount >= 3 || fiveCount >= 3 || sixCount >= 3) {
-			System.out.println("3カード : " + (1 * oneCount + 2 * twoCount + 
-					3 * threeCount + 4 * fourCount + 5 * fiveCount + 6 * sixCount)); 
-		}
-		
-		//同じ出目が4つ以上なら出目の和
-		if(oneCount >= 4 || twoCount >= 4 || threeCount >= 4 ||
-				fourCount >= 4 || fiveCount >= 4 || sixCount >= 4) {
-			System.out.println("4カード : " + (1 * oneCount + 2 * twoCount + 
-					3 * threeCount + 4 * fourCount + 5 * fiveCount + 6 * sixCount)); 
-		}
-		
-		//出目がAABBBかAAABBなら25点
-		if(oneCount == 0 || oneCount == 2 || oneCount == 3) {
-			if(twoCount == 0 || twoCount == 2 || twoCount == 3) {
-				if(threeCount == 0 || threeCount == 2 || threeCount == 3) {
-					if(fourCount == 0 || fourCount == 2 || fourCount == 3) {
-						if(fiveCount == 0 || fiveCount == 2 || fiveCount == 3) {
-							if(sixCount == 0 || sixCount == 2 || sixCount == 3) {
-								System.out.println("フルハウス : " + 25); 
-							}
-						}
-					}
+		for(int i = 0; i < 5; i++) { 
+			for(int j = 1; j <= 6; j++) {
+				if (dices[i] == j) {
+					numCounts[j - 1]++;
 				}
 			}
 		}
 		
-		//1234か2345か3456の出目なら30点
-		if((oneCount >= 1 && twoCount >= 1 && threeCount >= 1 && fourCount >= 1) ||
-			(twoCount >= 1 && threeCount >= 1 && fourCount >= 1 && fiveCount >= 1) ||
-			(threeCount >= 1 && fourCount >= 1 && fiveCount >= 1 && sixCount >= 1)) {
+		//1の目の合計
+		if(numCounts[0] >= 1) {
+			System.out.println("1の目: " + (1 * numCounts[0])); 
+		}
+		if(numCounts[1] >= 1) {
+			System.out.println("2の目: " + (2 * numCounts[1]));
+		}
+		if(numCounts[2] >= 1) {
+			System.out.println("3の目: " + (3 * numCounts[2]));
+		}
+		if(numCounts[3] >= 1) {
+			System.out.println("4の目: " + (4 * numCounts[3]));
+		}
+		if(numCounts[4] >= 1) {
+			System.out.println("5の目: " + (5 * numCounts[4]));
+		}
+		if(numCounts[5] >= 1) {
+			System.out.println("6の目: " + (6 * numCounts[5]));
+		}
+		
+		//出目の和
+		System.out.println("チャンス: " + (1 * numCounts[0] + 2 * numCounts[1] + 
+				3 * numCounts[2] + 4 * numCounts[3] + 5 * numCounts[4] + 6 * numCounts[5])); 
+		
+		//出目がAAAXXなら出目の和
+		if(numCounts[0] >= 3 || numCounts[1] >= 3 || numCounts[2] >= 3 || 
+				numCounts[3] >= 3 || numCounts[4] >= 3 || numCounts[5] >= 3) {
+			System.out.println("3カード : " + (1 * numCounts[0] + 2 * numCounts[1] + 
+					3 * numCounts[2] + 4 * numCounts[3] + 5 * numCounts[4] + 6 * numCounts[5])); 
+		}
+		
+		//出目がAAAAXなら出目の和
+		if(numCounts[0] >= 4 || numCounts[1] >= 4 || numCounts[2] >= 4 ||
+				numCounts[3] >= 4 || numCounts[4] >= 4 || numCounts[5] >= 4) {
+			System.out.println("4カード : " + (1 * numCounts[0] + 2 * numCounts[1] + 
+					3 * numCounts[2] + 4 * numCounts[3] + 5 * numCounts[4] + 6 * numCounts[5])); 
+		}
+		
+		//出目がAABBBかAAAAAなら25点
+		boolean a = (numCounts[0] == 0 || numCounts[0] == 2 || numCounts[0] == 3 || numCounts[0] == 5);
+		boolean b = (numCounts[1] == 0 || numCounts[1] == 2 || numCounts[1] == 3 || numCounts[1] == 5);
+		boolean c = (numCounts[2] == 0 || numCounts[2] == 2 || numCounts[2] == 3 || numCounts[2] == 5);
+		boolean d = (numCounts[3] == 0 || numCounts[3] == 2 || numCounts[3] == 3 || numCounts[3] == 5);
+		boolean e = (numCounts[4] == 0 || numCounts[4] == 2 || numCounts[4] == 3 || numCounts[4] == 5);
+		boolean f = (numCounts[5] == 0 || numCounts[5] == 2 || numCounts[5] == 3 ||numCounts[5] == 5);
+		if (a && b && c && d && e && f) {
+			System.out.println("フルハウス : " + 25);
+		}
+		
+		//出目が1234Xか2345Xか3456Xなら30点
+		boolean g = (numCounts[0] >= 1 && numCounts[1] >= 1 && numCounts[2] >= 1 && numCounts[3] >= 1);
+		boolean h = (numCounts[1] >= 1 && numCounts[2] >= 1 && numCounts[3] >= 1 && numCounts[4] >= 1);
+		boolean i = (numCounts[2] >= 1 && numCounts[3] >= 1 && numCounts[4] >= 1 && numCounts[5] >= 1);
+		if(g || h || i) {
 			System.out.println("Sスト : " + 30); 
 		}
 		
-		//12345か23456の出目なら40点
-		if((oneCount == 1 && twoCount == 1 && threeCount == 1 && fourCount == 1 && fiveCount == 1) ||
-				(twoCount == 1 && threeCount == 1 && fourCount == 1 && fiveCount == 1 || sixCount == 1)) {
-				System.out.println("Bスト : " + 40); 
-			}
+		//出目が12345か23456なら40点
+		boolean j = (numCounts[0] == 1 && numCounts[1] == 1 && numCounts[2] == 1 && numCounts[3] == 1 && numCounts[4] == 1);
+		boolean k = (numCounts[1] == 1 && numCounts[2] == 1 && numCounts[3] == 1 && numCounts[4] == 1 || numCounts[5] == 1);
+		if(j || k) {
+			System.out.println("Bスト : " + 40); 
+		}
 		
-		//同じ出目が5つ以上なら50点　※AAAAAもフルハウス扱いで25点
-		if(oneCount == 5 || twoCount == 5 || threeCount == 5 ||
-				fourCount == 5 || fiveCount == 5 || sixCount == 5) {
-			System.out.println("フルハウス : " + 25); 
+		//同じ出目が5つ以上なら50点
+		if(numCounts[0] == 5 || numCounts[1] == 5 || numCounts[2] == 5 ||
+				numCounts[3] == 5 || numCounts[4] == 5 || numCounts[5] == 5) {
 			System.out.println("ヨット : " + 50); 
 		}	
 	}
